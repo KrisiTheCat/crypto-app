31b855e90f59eec0a978bdf7e5fb28d2574b8e6a
KrisiTheCat <105876263+KrisiTheCat@users.noreply.github.com>
2025-04-18 23:06:32 +0300

    Update crypto_ms.sql

--- crypto_ms.sql
+++ crypto_ms.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Време на генериране: 16 апр 2025 в 19:47
+-- Време на генериране: 18 апр 2025 в 20:01
 -- Версия на сървъра: 8.2.0
 -- Версия на PHP: 7.2.34
 
@@ -42,21 +42,21 @@ CREATE TABLE IF NOT EXISTS `crypto` (
 INSERT INTO `crypto` (`id`, `name`, `symbol`) VALUES
 (1, 'Bitcoin', 'BTC'),
 (2, 'Ethereum', 'ETH'),
-(3, 'Tether', 'USDT'),
-(4, 'XRP', 'XRP'),
-(5, 'BNB', 'BNB'),
-(6, 'Solana', 'SOL'),
-(7, 'USDC', 'USDC'),
-(8, 'TRON', 'TRX'),
-(9, 'Dogecoin', 'DOGE'),
-(10, 'Cardano', 'ADA'),
-(11, 'Wrapped Bitcoin', 'WBTC'),
-(12, 'LEO Token', 'LEO'),
-(13, 'Chainlink', 'LINK'),
-(14, 'USDS', 'USDS'),
-(15, 'Stellar', 'XLM'),
-(16, 'Monero', 'XMR'),
-(17, 'Uniswap', 'UNI'),
+(3, 'Monero', 'XMR'),
+(4, 'Solana', 'SOL'),
+(5, 'XRP', 'XRP'),
+(6, 'Wrapped Bitcoin', 'WBTC'),
+(7, 'Binance Coin USD', 'BUSD'),
+(8, 'Chainlink', 'LINK'),
+(9, 'USDC', 'USDC'),
+(10, 'TRON', 'TRX'),
+(11, 'Dogecoin', 'DOGE'),
+(12, 'Cardano', 'ADA'),
+(13, 'Uniswap', 'UNI'),
+(14, 'Tether', 'USDT'),
+(15, 'LEO Token', 'LEO'),
+(16, 'USDS', 'USDS'),
+(17, 'Stellar', 'XLM'),
 (18, 'Aeve', 'AAVE'),
 (19, 'Bittensor', 'TAO'),
 (20, 'Maker', 'MKR');
@@ -87,15 +87,15 @@ INSERT INTO `crypto_seq` (`next_val`) VALUES
 
 DROP TABLE IF EXISTS `holding`;
 CREATE TABLE IF NOT EXISTS `holding` (
-  `id` bigint NOT NULL,
+  `id` bigint NOT NULL AUTO_INCREMENT,
   `investment` double DEFAULT NULL,
   `quantity` double DEFAULT NULL,
   `crypto_id` bigint DEFAULT NULL,
   `user_id` bigint DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `FK4piakq1b5cv13slxmbxtobjr0` (`crypto_id`),
-  KEY `FKqj6c499yadk3ka6gakt0t527s` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  KEY `holding_crypto_id` (`crypto_id`),
+  KEY `holding_user_id` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -113,7 +113,7 @@ CREATE TABLE IF NOT EXISTS `holding_seq` (
 --
 
 INSERT INTO `holding_seq` (`next_val`) VALUES
-(1351);
+(1);
 
 -- --------------------------------------------------------
 
@@ -123,17 +123,17 @@ INSERT INTO `holding_seq` (`next_val`) VALUES
 
 DROP TABLE IF EXISTS `transaction`;
 CREATE TABLE IF NOT EXISTS `transaction` (
-  `id` bigint NOT NULL,
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `crypto_id` bigint DEFAULT NULL,
   `date` datetime(6) DEFAULT NULL,
-  `method` tinyint DEFAULT NULL,
   `price` double NOT NULL,
   `quantity` double NOT NULL,
-  `crypto_id` bigint DEFAULT NULL,
   `user_id` bigint DEFAULT NULL,
+  `method` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `FK9qvcqovc8mo7kdapyl8rbc1jt` (`crypto_id`),
-  KEY `FKsg7jp0aj6qipr50856wf6vbw1` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  KEY `user_id` (`user_id`),
+  KEY `crypto_id` (`crypto_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -151,7 +151,7 @@ CREATE TABLE IF NOT EXISTS `transaction_seq` (
 --
 
 INSERT INTO `transaction_seq` (`next_val`) VALUES
-(1651);
+(1);
 
 -- --------------------------------------------------------
 
@@ -178,20 +178,6 @@ INSERT INTO `user` (`id`, `balance`, `name`, `password`) VALUES
 -- --------------------------------------------------------
 
 --
--- Структура на таблица `user_holdings`
---
-
-DROP TABLE IF EXISTS `user_holdings`;
-CREATE TABLE IF NOT EXISTS `user_holdings` (
-  `user_id` bigint NOT NULL,
-  `holdings_id` bigint NOT NULL,
-  UNIQUE KEY `UKigp76d7uclqfkthci7jdy9hud` (`holdings_id`),
-  KEY `FKjct535e895e22weeqriqa3tmy` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
-
---
 -- Структура на таблица `user_seq`
 --
 
@@ -207,20 +193,6 @@ CREATE TABLE IF NOT EXISTS `user_seq` (
 INSERT INTO `user_seq` (`next_val`) VALUES
 (1);
 
--- --------------------------------------------------------
-
---
--- Структура на таблица `user_transactions`
---
-
-DROP TABLE IF EXISTS `user_transactions`;
-CREATE TABLE IF NOT EXISTS `user_transactions` (
-  `user_id` bigint NOT NULL,
-  `transactions_id` bigint NOT NULL,
-  UNIQUE KEY `UKk12ybnbbgjjr7jr9d2831xoua` (`transactions_id`),
-  KEY `FK7ow9tl4pncou0lojsgqej6sce` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
 --
 -- Ограничения за дъмпнати таблици
 --
@@ -230,28 +202,16 @@ CREATE TABLE IF NOT EXISTS `user_transactions` (
 --
 ALTER TABLE `holding`
   ADD CONSTRAINT `FK4piakq1b5cv13slxmbxtobjr0` FOREIGN KEY (`crypto_id`) REFERENCES `crypto` (`id`),
-  ADD CONSTRAINT `FKqj6c499yadk3ka6gakt0t527s` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+  ADD CONSTRAINT `FKqj6c499yadk3ka6gakt0t527s` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  ADD CONSTRAINT `holding_crypto_id` FOREIGN KEY (`crypto_id`) REFERENCES `crypto` (`id`),
+  ADD CONSTRAINT `holding_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
 
 --
 -- Ограничения за таблица `transaction`
 --
 ALTER TABLE `transaction`
-  ADD CONSTRAINT `FK9qvcqovc8mo7kdapyl8rbc1jt` FOREIGN KEY (`crypto_id`) REFERENCES `crypto` (`id`),
-  ADD CONSTRAINT `FKsg7jp0aj6qipr50856wf6vbw1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
-
---
--- Ограничения за таблица `user_holdings`
---
-ALTER TABLE `user_holdings`
-  ADD CONSTRAINT `FKc5xhp2gaay3nr828o43hk17l5` FOREIGN KEY (`holdings_id`) REFERENCES `holding` (`id`),
-  ADD CONSTRAINT `FKjct535e895e22weeqriqa3tmy` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
-
---
--- Ограничения за таблица `user_transactions`
---
-ALTER TABLE `user_transactions`
-  ADD CONSTRAINT `FK7ow9tl4pncou0lojsgqej6sce` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
-  ADD CONSTRAINT `FKli5uya1wifm860dqhjke7kyuj` FOREIGN KEY (`transactions_id`) REFERENCES `transaction` (`id`);
+  ADD CONSTRAINT `transaction_crypto_id` FOREIGN KEY (`crypto_id`) REFERENCES `crypto` (`id`),
+  ADD CONSTRAINT `transaction_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
